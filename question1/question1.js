050f0cfb01b57b545e85a23c3fb10201c0bac913
AbdullahAlRasheedi <alkw444@gmail.com>
2021-01-03 20:54:44 +0300

    cw6 completed

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,11 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+let name = prompt("what is your name: ");
+function sayHello(){
+    if(name.length == 0){
+        alert(`hello friend`);
+    }
+    else{
+    alert(`hello ${name}`);
+    }
+}
+sayHello()
